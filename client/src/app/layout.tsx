ca053caaf4a6129885b0f8b257564de3d5234de2
Alexander <sashalexjr@gmail.com>
2025-12-01 21:35:35 +0500

    - init commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import {Inter} from "next/font/google";
+import "./globals.css";
+import {ReactNode} from "react";
+
+const inter = Inter({
+  subsets: ["latin", "cyrillic"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "eduWiki",
+  description: "О учёбе за границей",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
