80c618785f4776c2811948ab66c4d21dd81a991e
Alexander <sashalexjr@gmail.com>
2025-12-21 18:24:15 +0500

    fix: add AuthModule import to countries.module.ts feat: add AuthGuard to AddCountry

--- backend/src/countries/countries.controller.ts
+++ backend/src/countries/countries.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CountriesService } from './countries.service.js';
 import { CreateCountryDto } from './DTO/countries.dto.js';
+import { AuthGuard } from '../auth/auth.guard.js';
 
 @Controller('countries')
 export class CountriesController {
@@ -11,6 +12,7 @@ export class CountriesController {
     return this.countriesService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Post('/add')
   addCountry(@Body() dto: CreateCountryDto) {
     return this.countriesService.addCountry(dto);
--- backend/src/countries/countries.module.ts
+++ backend/src/countries/countries.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CountriesController } from './countries.controller.js';
 import { CountriesService } from './countries.service.js';
 import { PrismaService } from '../prisma.service.js';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [CountriesController],
   providers: [CountriesService, PrismaService],
 })
